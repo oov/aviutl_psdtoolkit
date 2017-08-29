8b29b47f973fcbe37ce641af0d6c17521f0b14ea
oov <ilovevipper@gmail.com>
2017-08-29 15:31:15 +0900

    add build script

---
+++ build.bat
@@ -0,0 +1,16 @@
+@rem copy script files
+copy /B /Y src\lua\PSDToolKitLib.lua bin\script\PSDToolKit\
+copy /B /Y src\lua\@PSDToolKit.anm bin\script\PSDToolKit\
+
+@rem build src/go/assets/bindata.go
+cd src/go/assets
+go generate
+cd ../../..
+
+@rem build PSDToolKit.exe
+cd src/go
+go build -ldflags="-s -H=windowsgui" -o ../../bin/script/PSDToolKit/PSDToolKit.exe
+cd ../../
+
+@rem build lazarus projects
+C:\lazarus\lazbuild.exe --build-all src/lazarus/auf.lpi src/lazarus/luadll.lpi
