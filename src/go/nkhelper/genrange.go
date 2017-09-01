e5c7ea2f4062e6a32a31c511066f286ea0402bda
oov <ilovevipper@gmail.com>
2017-09-01 18:14:53 +0900

    add some chars

--- src/go/nkhelper/genrange.go
+++ src/go/nkhelper/genrange.go
@@ -16,7 +16,6 @@ import (
 var source = `#ifndef JPRANGE_H____
 #define JPRANGE_H____
 const nk_rune nk_font_japanese_glyph_ranges[] = {
-0x0020, 0x007f,
 {{range .ranges}}{{printf "0x%04x, 0x%04x,\n" (index . 0) (index . 1)}}{{end}}0
 };
 #endif
@@ -51,6 +50,22 @@ func main() {
 			m[i] = struct{}{}
 		}
 	}
+	// additional ranges
+	// reference: https://encoding.spec.whatwg.org/#shift_jis-encoder
+	for _, r := range [][2]uint32{
+		{0x0020, 0x0080}, // ASCII
+		{0xFF61, 0xFF9F}, // Halfwidth Katakana
+
+		{0x00A5, 0x00A5}, // YEN SIGN
+		{0x203e, 0x203e}, // OVERLINE
+		{0x2212, 0x2212}, // MINUS SIGN
+		{0xFF0D, 0xFF0D}, // FULLWIDTH HYPHEN-MINUS
+	} {
+		for i, j := r[0], r[1]; i <= j; i++ {
+			m[i] = struct{}{}
+		}
+	}
+
 	a := []uint32{}
 	for v := range m {
 		a = append(a, v)
