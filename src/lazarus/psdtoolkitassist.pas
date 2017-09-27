b99ea89d5a0cc6cfe6aeda44926745cde8aa8f85
oov <ilovevipper@gmail.com>
2017-09-28 03:29:38 +0900

    bump version

--- src/lazarus/psdtoolkitassist.pas
+++ src/lazarus/psdtoolkitassist.pas
@@ -118,7 +118,7 @@ end;
 constructor TPSDToolKitAssist.Create;
 const
   PluginName = 'PSDToolKit';
-  PluginInfo = 'PSDToolKitAssist v0.1.2rc2';
+  PluginInfo = 'PSDToolKitAssist v0.1.2rc3';
 begin
   inherited Create();
   FillChar(FEntry, SizeOf(FEntry), 0);
