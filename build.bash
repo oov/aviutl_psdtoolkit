5b1c05ca5cb1f17a58a2411ae59e83f08b01460b
oov <ilovevipper@gmail.com>
2017-11-03 06:24:52 +0900

    bump version

--- build.bash
+++ build.bash
@@ -6,7 +6,7 @@ mkdir bin bin/PSDToolKit bin/script bin/script/PSDToolKit bin/GCMZDrops bin/GCMZ
 sed 's/\r$//' README.md | sed 's/$/\r/' > bin/README.txt
 
 # update version string
-VERSION='v0.1.2'
+VERSION='v0.1.3beta'
 GITHASH=`git rev-parse --short HEAD`
 cat << EOS | sed 's/\r$//' | sed 's/$/\r/' > 'src/lazarus/ver.pas'
 unit Ver;
