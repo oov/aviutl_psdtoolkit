cb62852761619002dc3cbaf3b6bb2e37076c954b
oov <ilovevipper@gmail.com>
2018-01-26 03:13:51 +0900

    remove unnecessary nil checks

--- src/go/gui/mainview/mainview.go
+++ src/go/gui/mainview/mainview.go
@@ -112,6 +112,20 @@ func (mv *MainView) Clear() {
 }
 
 func (mv *MainView) Render(ctx *nk.Context) {
+eat:
+	for {
+		select {
+		case f := <-mv.queue:
+			f()
+		default:
+			break eat
+		}
+	}
+
+	if mv.resizedImage == nil {
+		return
+	}
+
 	rgn := nk.NkWindowGetContentRegion(ctx)
 	winHeight := rgn.H()
 	const (
@@ -132,40 +146,24 @@ func (mv *MainView) Render(ctx *nk.Context) {
 				mv.zooming = true
 			}
 			mv.zoom = z
-			if mv.resizedImage != nil {
-				mv.updateViewImage(true)
-			}
+			mv.updateViewImage(true)
 		} else if (ctx.LastWidgetState()&nk.WidgetStateActive) != nk.WidgetStateActive && mv.zooming {
 			mv.zooming = false
-			if mv.resizedImage != nil {
-				mv.updateViewImage(false)
-			}
+			mv.updateViewImage(false)
 		}
 		nk.NkGroupEnd(ctx)
 	}
 }
 
 func (mv *MainView) renderCanvas(ctx *nk.Context) {
-eat:
-	for {
-		select {
-		case f := <-mv.queue:
-			f()
-		default:
-			break eat
-		}
-	}
-
 	rgn := nk.NkWindowGetContentRegion(ctx)
 	winWidth, winHeight := int(rgn.W()), int(rgn.H())
 
 	resizedImage := mv.resizedImage
 	forceUpdate := mv.forceUpdate
 	forceFitToWindow := mv.forceFitToWindow
-	if resizedImage != nil {
-		mv.forceUpdate = false
-		mv.forceFitToWindow = false
-	}
+	mv.forceUpdate = false
+	mv.forceFitToWindow = false
 
 	zoom := mv.zoom
 
@@ -181,20 +179,18 @@ eat:
 		winWidth,
 		winHeight,
 	)
-	if resizedImage != nil {
-		imageRect.Max.X += int(float64(resizedImage.Rect.Dx()) * zr)
-		imageRect.Max.Y += int(float64(resizedImage.Rect.Dy()) * zr)
-	}
+	imageRect.Max.X += int(float64(resizedImage.Rect.Dx()) * zr)
+	imageRect.Max.Y += int(float64(resizedImage.Rect.Dy()) * zr)
 	workspaceSize := image.Pt(
 		winWidth*2+imageRect.Dx(),
 		winHeight*2+imageRect.Dy(),
 	)
 
-	if !workspaceSize.Eq(mv.latestWorkspaceSize) || (resizedImage != nil && forceFitToWindow) {
+	if !workspaceSize.Eq(mv.latestWorkspaceSize) || forceFitToWindow {
 		latestWinWidth := (mv.latestWorkspaceSize.X - mv.latestImageRect.Dx()) / 2
 		latestWinHeight := (mv.latestWorkspaceSize.Y - mv.latestImageRect.Dy()) / 2
 		var x, y float64
-		if resizedImage != nil && forceFitToWindow {
+		if forceFitToWindow {
 			x = 0.5
 			y = 0.5
 		} else {
@@ -211,67 +207,66 @@ eat:
 
 	activeRect := image.Rect(int(mv.scrollX), int(mv.scrollY), int(mv.scrollX)+int(rgn.W()), int(mv.scrollY)+int(rgn.H()))
 
-	if resizedImage != nil {
-		if !activeRect.Eq(mv.latestActiveRect) || !workspaceSize.Eq(mv.latestWorkspaceSize) || forceUpdate {
-			if activeRect.Dx() > mv.visibleAreaImage.Rect.Dx() || activeRect.Dy() > mv.visibleAreaImage.Rect.Dy() {
-				var textureSize int
-				if activeRect.Dx() > activeRect.Dy() {
-					textureSize = int(math.Pow(2, math.Ceil(math.Log(float64(activeRect.Dx()))/math.Ln2)))
-				} else {
-					textureSize = int(math.Pow(2, math.Ceil(math.Log(float64(activeRect.Dy()))/math.Ln2)))
-				}
-				mv.visibleAreaImage = image.NewNRGBA(image.Rect(0, 0, textureSize, textureSize))
+	if !activeRect.Eq(mv.latestActiveRect) || !workspaceSize.Eq(mv.latestWorkspaceSize) || forceUpdate {
+		if activeRect.Dx() > mv.visibleAreaImage.Rect.Dx() || activeRect.Dy() > mv.visibleAreaImage.Rect.Dy() {
+			var textureSize int
+			if activeRect.Dx() > activeRect.Dy() {
+				textureSize = int(math.Pow(2, math.Ceil(math.Log(float64(activeRect.Dx()))/math.Ln2)))
+			} else {
+				textureSize = int(math.Pow(2, math.Ceil(math.Log(float64(activeRect.Dy()))/math.Ln2)))
 			}
-			blend.Clear.Draw(mv.visibleAreaImage, image.Rect(0, 0, activeRect.Dx(), activeRect.Dy()), image.Transparent, image.Point{})
-			ix := int(float64(activeRect.Min.X-(workspaceSize.X-imageRect.Dx())/2) * izr)
-			iy := int(float64(activeRect.Min.Y-(workspaceSize.Y-imageRect.Dy())/2) * izr)
-			draw.NearestNeighbor.Scale(
-				mv.visibleAreaImage,
-				image.Rect(0, 0, activeRect.Dx(), activeRect.Dy()),
-				resizedImage,
-				image.Rect(ix, iy, ix+int(float64(activeRect.Dx())*izr), iy+int(float64(activeRect.Dy())*izr)),
-				draw.Src,
-				nil,
-			)
-			mv.visibleArea.Update(mv.visibleAreaImage)
+			mv.visibleAreaImage = image.NewNRGBA(image.Rect(0, 0, textureSize, textureSize))
 		}
-
-		// background animation
-		nk.NkLayoutSpacePush(ctx, nk.NkRect(
-			float32(imageRect.Min.X),
-			float32(imageRect.Min.Y),
-			float32(imageRect.Dx()-1),
-			float32(imageRect.Dy()-1),
-		))
-		cmdbuf := nk.NkWindowGetCanvas(ctx)
-		var widgetRect nk.Rect
-		if nk.NkWidget(&widgetRect, ctx) != 0 {
-			var col nk.Color
-			col.SetRGBA(255, 255, 255, 255)
-			h := float32(256 - 16)
-			for y, yEnd := widgetRect.Y(), widgetRect.Y()+widgetRect.H(); y < yEnd; y += h {
-				if y+h > yEnd {
-					h = yEnd - y
-				}
-				w := float32(256 - 16)
-				for x, xEnd := widgetRect.X(), widgetRect.X()+widgetRect.W(); x < xEnd; x += w {
-					if x+w > xEnd {
-						w = xEnd - x
-					}
-					img := mv.bg.SubImage(nk.NkRect(16-mv.bgScroll, 16-mv.bgScroll, w, h))
-					nk.NkDrawImage(cmdbuf, nk.NkRect(x, y, w, h), &img, col)
-				}
-			}
-		}
-		mv.bgScroll += 0.5
-		if mv.bgScroll > 8 {
-			mv.bgScroll -= 8
-		}
-
-		img := mv.visibleArea.SubImage(nk.NkRect(0, 0, rgn.W(), rgn.H()))
-		nk.NkLayoutSpacePush(ctx, nk.NkRect(float32(mv.scrollX), float32(mv.scrollY), rgn.W(), rgn.H()))
-		nk.NkImage(ctx, img)
+		blend.Clear.Draw(mv.visibleAreaImage, image.Rect(0, 0, activeRect.Dx(), activeRect.Dy()), image.Transparent, image.Point{})
+		ix := int(float64(activeRect.Min.X-(workspaceSize.X-imageRect.Dx())/2) * izr)
+		iy := int(float64(activeRect.Min.Y-(workspaceSize.Y-imageRect.Dy())/2) * izr)
+		draw.NearestNeighbor.Scale(
+			mv.visibleAreaImage,
+			image.Rect(0, 0, activeRect.Dx(), activeRect.Dy()),
+			resizedImage,
+			image.Rect(ix, iy, ix+int(float64(activeRect.Dx())*izr), iy+int(float64(activeRect.Dy())*izr)),
+			draw.Src,
+			nil,
+		)
+		mv.visibleArea.Update(mv.visibleAreaImage)
 	}
+
+	// background animation
+	nk.NkLayoutSpacePush(ctx, nk.NkRect(
+		float32(imageRect.Min.X),
+		float32(imageRect.Min.Y),
+		float32(imageRect.Dx()-1),
+		float32(imageRect.Dy()-1),
+	))
+	cmdbuf := nk.NkWindowGetCanvas(ctx)
+	var widgetRect nk.Rect
+	if nk.NkWidget(&widgetRect, ctx) != 0 {
+		var col nk.Color
+		col.SetRGBA(255, 255, 255, 255)
+		h := float32(256 - 16)
+		for y, yEnd := widgetRect.Y(), widgetRect.Y()+widgetRect.H(); y < yEnd; y += h {
+			if y+h > yEnd {
+				h = yEnd - y
+			}
+			w := float32(256 - 16)
+			for x, xEnd := widgetRect.X(), widgetRect.X()+widgetRect.W(); x < xEnd; x += w {
+				if x+w > xEnd {
+					w = xEnd - x
+				}
+				img := mv.bg.SubImage(nk.NkRect(16-mv.bgScroll, 16-mv.bgScroll, w, h))
+				nk.NkDrawImage(cmdbuf, nk.NkRect(x, y, w, h), &img, col)
+			}
+		}
+	}
+	mv.bgScroll += 0.5
+	if mv.bgScroll > 8 {
+		mv.bgScroll -= 8
+	}
+
+	img := mv.visibleArea.SubImage(nk.NkRect(0, 0, rgn.W(), rgn.H()))
+	nk.NkLayoutSpacePush(ctx, nk.NkRect(float32(mv.scrollX), float32(mv.scrollY), rgn.W(), rgn.H()))
+	nk.NkImage(ctx, img)
+
 	nk.NkLayoutSpaceEnd(ctx)
 	mv.latestImageRect = imageRect
 	mv.latestActiveRect = activeRect
