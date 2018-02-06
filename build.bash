c23b95355fa17bc773dceecbcab768cc0e58916d
oov <ilovevipper@gmail.com>
2018-02-06 18:53:57 +0900

    add application icon

--- build.bash
+++ build.bash
@@ -65,6 +65,7 @@ popd
 
 # build PSDToolKit.exe
 pushd src/go
+rsrc -ico assets/data/icon.ico -arch=amd64 -o PSDToolKit.syso
 go.exe build -x -tags gdip -ldflags="-s" -o ../../bin/script/PSDToolKit/PSDToolKit.exe
 popd
 
