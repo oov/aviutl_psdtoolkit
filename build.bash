57050a7a55686c8e1d54d9df028041edd9f9e1e9
oov <ilovevipper@gmail.com>
2020-06-25 23:20:51 +0900

    follow forcepser update

--- build.bash
+++ build.bash
@@ -92,6 +92,7 @@ cmd.exe /c C:/lazarus/lazbuild.exe --build-all src/lazarus/ictalk/ictalk.lpi
 # copy forcepser release version
 # cp ../forcepser/bin/forcepser.* bin/かんしくん/
 # cp ../forcepser/bin/setting.txt-template bin/かんしくん/
+# cp ../forcepser/bin/template.exo-template bin/かんしくん/
 # cp ../forcepser/bin/_entrypoint.lua bin/かんしくん/
 
 # install
