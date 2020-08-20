187ead090548af6da57710e40003ef7136fe8675
oov <ilovevipper@gmail.com>
2020-08-21 00:23:29 +0900

    bump version

---
+++ pack.bat
@@ -0,0 +1,11 @@
+@echo off
+cd bin
+..\7za.exe a -tzip ..\psdtoolkit.zip ^
+  GCMZDrops.* GCMZDrops ^
+  RelMovieHandle.* ^
+  ZRamPreview.* ^
+  Ç©ÇÒÇµÇ≠ÇÒ ^
+  AudioMixer.auf ^
+  PSDToolKit.* PSDToolKit script ^
+  PSDToolKitê‡ñæèë.html PSDToolKitDocs
+cd ..
