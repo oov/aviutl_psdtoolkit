b94ec1460a62e243d5feaeaf1af60b0ee896999e
oov <ilovevipper@gmail.com>
2021-01-19 03:36:02 +0900

    update forcepser to 0.1rc4

--- build.bash
+++ build.bash
@@ -88,6 +88,7 @@ cmd.exe /c C:/lazarus/lazbuild.exe --build-all src/lazarus/AudioMixerPlugin.lpi
 # copy forcepser release version
 # cp ../forcepser/bin/forcepser.* bin/かんしくん/
 # cp ../forcepser/bin/setting.txt-template bin/かんしくん/
+# cp ../forcepser/bin/setting.txt-template-old bin/かんしくん/
 # cp ../forcepser/bin/_entrypoint.lua bin/かんしくん/
 # cp ../forcepser/bin/asas/* bin/かんしくん/asas/
 
